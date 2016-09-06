ec08557a323fcc24abe638c7f1312b1083046651
Jim Bulkowski <jim.b@paperelectron.com>
2016-09-06 12:47:47 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,49 @@
+/**
+ * @file index
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project Pomegranate-express-html
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+'use strict';
+
+/**
+ *
+ * @module index
+ */
+
+'use strict';
+
+/**
+ *
+ * @module index
+ */
+
+exports.options = {
+  workDir: './views'
+}
+
+exports.metadata = {
+  name: 'HtmlViews',
+  type: 'merge',
+  param: 'ExpressConfig'
+}
+
+exports.plugin = {
+  load: function(inject, loaded) {
+    var viewDirectory = this.options.workDir;
+    var viewEngine = {
+      "View Engine": function(Express){
+        Express.set('views', viewDirectory)
+        Express.set('view engine', 'html')
+      }
+    }
+    loaded(null, viewEngine)
+  },
+  start: function(done) {
+    done()
+  },
+  stop: function(done) {
+    done()
+  }
+}
